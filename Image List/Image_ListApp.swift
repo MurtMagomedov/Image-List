90ab0b96fe443e736ee909a4a797d60a9c2a54ea
Murtazali <m.magomedov.a@mail.ru>
2024-12-02 10:43:01 +0300

    Initial Commit

---
+++ Image List/Image_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image_ListApp.swift
+//  Image List
+//
+//  Created by Муртазали Магомедов on 02.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct Image_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
